4f0937e885df65d4c88f18245d39192f8c3bb8fa
Johnny Proano <djimageonline@gmail.com>
2023-01-27 16:56:15 -0500

    completed loops2 practice exercise in JavaScript

--- loops2.js
+++ loops2.js
@@ -1,30 +1,80 @@
 // # 1. Write a while loop to print the numbers 1 through 10.
 
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
 // # -----------------------------------------------------------------
 // # 2. Write a while loop that prints the word "hello" 5 times.
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Hello");
+// }
 
 // # -----------------------------------------------------------------
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
+// let word = "";
+// while (word !== "stop") {
+//   word = prompt("Enter another word");
+// }
+
 // # -----------------------------------------------------------------
 // # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+// let count = 0;
+// while (count < 100) {
+//   count += 5;
+//   console.log(count);
+// }
 
 // # -----------------------------------------------------------------
 // # 5. Write a while loop that prints the number 9000 ten times.
 
+// let count = 1;
+// while (count <= 10) {
+//   console.log(9000);
+//   count += 1;
+// }
+
 // # -----------------------------------------------------------------
 // # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// let num = 0;
+// while (num < 10) {
+//   num = prompt("Enter another num");
+// }
 
 // # -----------------------------------------------------------------
 // # 7. Write a while loop that prints the numbers 50 to 70.
 
+// for (let i = 50; i >= 50 && i <= 70; i++) {
+//   console.log(i);
+//   i += 1;
+// }
+
 // # -----------------------------------------------------------------
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+// for (let phrase = 1; phrase <= 144; phrase++) {
+//   console.log(`${phrase}: Around the World`);
+// }
 
 // # -----------------------------------------------------------------
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
+// let word = "";
+// while (word.length <= 5) {
+//   word = prompt("Keep going");
+// }
+
 // # -----------------------------------------------------------------
 // # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+// let even = 2;
+// while (even <= 40) {
+//   console.log(even);
+//   even += 2;
+// }
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
