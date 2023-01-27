97dcd43de80c73ee4cbef930b3b8afd88a4aae60
Johnny Proano <djimageonline@gmail.com>
2023-01-26 20:51:28 -0500

    completed JS Strings exercise

--- strings.js
+++ strings.js
@@ -1,40 +1,77 @@
-// # # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+//# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// let first = "Maria";
+// let last = "Michaels";
+
+// console.log("Hi My name is" + " " + first + " " + last);
 
 // ######################################################################
-
 // # # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-// ######################################################################
+// let fullName = "Jake Paul";
+// console.log(`Hi I am ${fullName}`);
 
+// ######################################################################
 // ## # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-// ######################################################################
+// let word = prompt("Enter a Word");
+// console.log(word);
 
+// if (word === "marco" ? console.log("polo") : null);
+
+// ######################################################################
 // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-// ######################################################################
+// let red = "red";
+// let blue = "blue";
+// let yellow = "yellow";
 
+// console.log("I love these: " + red + " " + blue + " " + yellow);
+
+// ######################################################################
 // ## 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-// ######################################################################
+// let green = "green";
+// let orange = "orange";
+// let pink = "pink";
 
+// console.log(`I love ${green}, ${orange} and finally ${pink}`);
+
+// ######################################################################
 // # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
-// ######################################################################
+// let name = prompt("enter a name");
+// if (name !== "Santa" ? console.log("You are not Santa") : console.log(`You are ${name}`));
 
+// ######################################################################
 // # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+// let book = "Harry Potter";
+// let author = "JK Rowlings";
+
+// console.log("This is my fav: " + book + " by " + author);
 
 // ######################################################################
-
 // # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
-// ######################################################################
+// let book = "Harry Potter";
+// let author = "JK Rowlings";
 
+// console.log(`I Love ${book} by ${author}`);
+
+// ######################################################################
 // # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
-// ######################################################################
+// let password = prompt("Enter a Password");
+// if (password === "Joshua" ? console.log("Shall we play a game") : console.log("Access Denied"));
 
+// ######################################################################
 // # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+let city1 = "Chi";
+let city2 = "Fl";
+let ciyt3 = "Tx";
+
+console.log("I love " + city1 + "," + city2 + "," + ciyt3);
 
 // ######################################################################
 
