10289ca8ed996bb288c4e07241acbad196bbd5da
Johnny Proano <djimageonline@gmail.com>
2023-01-26 23:21:04 -0500

    completed methods.js exercise

--- methods.js
+++ methods.js
@@ -1,33 +1,78 @@
 // # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+// let word = prompt("Enter a word");
+// word = word.toUpperCase();
+// console.log(word);
+
 // # ---------------------------------------------------------------------------------------
 
 // # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// let number = prompt("Enter a number");
+// if (number > 100 ? console.log("Thats a big number") : console.log("Play agian"));
 
 // # ---------------------------------------------------------------------------------------
 
 // # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+// let num1 = Number(prompt("Enter Two numbers.  Enter First"));
+// let num2 = Number(prompt("Enter Sec"));
+// console.log(`The total is ${num1 + num2}`);
+
 // # ---------------------------------------------------------------------------------------
 // # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+// prompt("Enter a word");
+
+// let word = "cow";
+// console.log(word.split("").reverse().join(""));
+
+// let word = "horse";
+
+// const revArray = [];
+// const length = word.length - 1;
+// for (let i = length; i >= 0; i--) {
+//   revArray.push(word[i]);
+// }
+// console.log(revArray.join(""));
 
 // # ---------------------------------------------------------------------------------------
 // # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+// let num = Number("10");
+// console.log(num * 10);
+
 // # ---------------------------------------------------------------------------------------
 // # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+// let word1 = "word";
+// let word2 = "big word";
+
+// console.log(`${word1.toUpperCase()} ${word2.toUpperCase()}`);
 
 // # ---------------------------------------------------------------------------------------
 // # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
+// let word = "mine";
+// console.log(word.length);
+
 // # ---------------------------------------------------------------------------------------
 // # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// let num = Number("1");
+// if (num < 0 ? console.log("Neg number") : null);
 
 // # ---------------------------------------------------------------------------------------
 // # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 * num2);
+
 // # ---------------------------------------------------------------------------------------
 // # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+// let word = prompt("Enter a word");
+// if (word.length > 5 ? console.log("Long word alert") : console.log("short word"));
 
 // # ---------------------------------------------------------------------------------------
 // # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -1,58 +1,93 @@
+
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-
+  # word =  gets.chomp.upcase
+  # puts word
 
 # ---------------------------------------------------------------------------------------
-
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
+# puts "Enter a number: "
+# number = gets.chomp.to_i
+# if number > 100
+#   puts "#{number} is big number"
+# else
+#   puts "Pick another number so this works right"
+# end
 
 # ---------------------------------------------------------------------------------------
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+# puts "Enter two numbers"
+# puts "First num:"
+# num1 = gets.chomp.to_i
+# puts "Second num:"
+# num2 = gets.chomp.to_i
+# puts "The total is #{num1 + num2}"
 
 # ---------------------------------------------------------------------------------------
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# puts "Enter a word:"
+# word = gets.chomp.reverse
+# puts word
 
 
 # ---------------------------------------------------------------------------------------
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+# puts "Enter Num"
+# num = gets.chomp.to_i
+# num *= 10
+# puts num
 
 
 # ---------------------------------------------------------------------------------------
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
-
-
-
+# puts "Enter 2 words.  First word:"
+# word = gets.chomp.upcase
+# puts "Sec word:"
+# word2 = gets.chomp.upcase
+# puts word + " " + word2
 
 # ---------------------------------------------------------------------------------------
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
-
-
+# puts "enter a word"
+# word = gets.chomp
+# puts word.length
 
 # ---------------------------------------------------------------------------------------
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
-
-
-
+# puts "Enter num"
+# num = gets.chomp.to_i
+# if num < 0 
+#   puts "#{num} is a negative num"
+# end
 
 
 # ---------------------------------------------------------------------------------------
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
 
-
-
+# puts "Enter two nums"
+# num1 = gets.chomp.to_i
+# num2 = gets.chomp.to_i
+# puts "The total is #{num1 * num2}"
 
 # ---------------------------------------------------------------------------------------
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
-
+# puts "Enter a word"
+# word = gets.chomp
+# if word.length > 5
+#   puts "thats huge"
+# else
+#   puts "cool size"
+# end
 
 
 
