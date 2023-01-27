bb4a69815760474437411d1ea98380403bcec07f
Johnny Proano <djimageonline@gmail.com>
2023-01-27 16:28:28 -0500

    completed loops2 practice exercise in Ruby

--- loops2_practice_problems.rb
+++ loops2_practice_problems.rb
@@ -1,12 +1,18 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
-
+# count = 1
+# while count <= 10
+#   p count
+#   count += 1
+# end
 
 
 # -----------------------------------------------------------------
 # 2. Write a while loop that prints the word "hello" 5 times.
 
-
+# 5.times do
+#   puts "hello"
+# end
 
 
 
@@ -14,48 +20,75 @@
 # -----------------------------------------------------------------
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
-
-
-
+# word = ""
+# while word != "stop" 
+#   word = gets.chomp
+# end
 
 # -----------------------------------------------------------------
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
+# num = 0
+# while num <= 100
+#   puts num
+#   num += 5
+# end
 
-
-# -----------------------------------------------------------------
+# ----------------------------------------------------------------
 # 5. Write a while loop that prints the number 9000 ten times.
 
-
-
+# 10.times do
+#   puts 9000
+# end
 
 
 # -----------------------------------------------------------------
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
+# num = ""
+# while num.to_i < 10
+#   num = gets.chomp.to_i
+# end
+# puts "#{num} is a high number"
 
 
 # -----------------------------------------------------------------
 # 7. Write a while loop that prints the numbers 50 to 70.
 
-
+# num = 50
+# while num >= 50 && num <= 70
+#   puts num
+#   num += 1
+# end
 
 
 # -----------------------------------------------------------------
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
-
+# count = 1
+# while count <= 144
+#   puts " #{count}: Around the world"
+#   count += 1
+# end
 
 
 # -----------------------------------------------------------------
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
-
+# word = ""
+# while word.length <= 5
+#   word = gets.chomp
+# end
 
 
 # -----------------------------------------------------------------
 # 10. Write a while loop that prints the even numbers from 2 to 40.
 
+# even = 2
+# while even <= 40
+#   puts even
+#   even += 2
+# end
 
 
 
