f4a2be943311bc1b1f3a15fef54fead396292191
Johnny Proano <djimageonline@gmail.com>
2023-01-27 20:34:57 -0500

    completed arrays1_practice_problems.rb in RUBY

--- arrays1_practice_problems.rb
+++ arrays1_practice_problems.rb
@@ -1,54 +1,90 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
+# words = ["cow", "rat", "mouse"]
+# words.push("dog", "cat")
+# words << "milk"
+# p words
 
 
 
 #-----------------------------------------------------------
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-
+# letters = ["a","b", "c", "d"]
+# letters[1] = 5
+# p letters
 
 
 #-----------------------------------------------------------
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+# numbers = [5, 6, 10, 12, 4]
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
+
  
 #-----------------------------------------------------------
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
+# arrays = [1]
+# arrays.push(2,4,5)
+# puts arrays
 
 
 #-----------------------------------------------------------
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# strings = ["cow", "mine", "shoes"]
+# strings[2] = strings[2].upcase
+# p strings
 
 
 
 #-----------------------------------------------------------
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
-
+# names = ["Joe", "maya", "Stef"]
+# index = 0
+# while index < names.length
+#   p names[index]
+#   index += 1
+# end
 
 #-----------------------------------------------------------
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
-
+# strings = ["piano", "cow"]
+# strings << "shoe"
+# p strings
 
 
 #-----------------------------------------------------------
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+# numbers = [4, 6, 2, 4, 1]
+# numbers[0] = 8
+# p numbers
 
 
 #-----------------------------------------------------------
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
-
-
+# nums = [4, 9]
+# index = 0
+# while index < nums.length
+#   p nums[index]
+#   index += 1
+# end
 
 #-----------------------------------------------------------
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
-
+# names = ["USA", "China", "Africa"]
+# names << "Australia"
+# p names
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
