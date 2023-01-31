8045136b2ae9e350a1969de3d84934f9711ca02f
Johnny Proano <jproano22@yahoo.com>
2023-01-31 15:43:05 -0500

    completed hash.js in Javascript

--- hash.js
+++ hash.js
@@ -32,22 +32,87 @@
 // #---------------------------------------
 // # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
+// let book = {
+//   title: "HP",
+//   author: "JK",
+//   numPages: 545,
+//   language: "english",
+// };
+
+// console.log(book.title);
+// console.log(book.author);
+// console.log(book.numPages);
+// console.log(book.language);
+
 // #---------------------------------------
 // # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+// let books = [
+//   { title: "HP", author: "JK" },
+//   { title: "Apple", author: "Mike" },
+//   { title: "Sscream", author: "Todd" },
+// ];
+
+// console.log(books[2].author);
 
 // #---------------------------------------
 // # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
+// let states = {
+//   Il: "Springfield",
+//   Fl: "Miami",
+//   Tx: "Austin",
+// };
+
+// states.Az = "tempe";
+// console.log(states);
+
 // #---------------------------------------
 // # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+// let laptops = {
+//   brand: "Apple",
+//   model: "ipad",
+//   year: 2019,
+// };
+
+// console.log(laptops.brand);
+// console.log(laptops.model);
+// console.log(laptops.year);
 
 // #---------------------------------------
 // # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
+// let laptops = [
+//   { brand: "Apple", model: "iphone" },
+//   { brand: "Samsung", model: "galaxy" },
+//   { brand: "Sony", model: "PS5" },
+// ];
+
+// console.log(laptops[1].model);
+
 // #---------------------------------------
 // # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
+// let words = {
+//   word1: "definition1",
+//   word2: "definition2",
+// };
+
+// words.word3 = "definition3";
+// console.log(words);
+
 // #---------------------------------------
 // # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+// let shirt = {
+//   brand: "fubu",
+//   color: "red",
+//   size: "large",
+// };
+
+// console.log(shirt.brand);
+// console.log(shirt.color);
+// console.log(shirt.size);
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/d257aec07882d78009bd796ed53f81bb
