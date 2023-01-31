162e0e7c39a14a0e95fcd713502bcb192baa4520
Johnny Proano <jproano22@yahoo.com>
2023-01-31 16:17:59 -0500

    completed functions practice in Ruby

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -2,63 +2,105 @@
 
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+# def times_two (num)
+#   return num * 2
+# end
 
-
-# ----------------------------------------------------------------------------------------
+# p times_two(5);
+# #----------------------------------------------------------------------------------------
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
+# def caps(string)
+#   return string.upcase
+# end
 
+# p caps("wow")
 
 # ----------------------------------------------------------------------------------------
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+# def subtract(num1, num2)
+#   return num2 - num1
+# end
+
+# p subtract(10, 8)
 
 
 # ----------------------------------------------------------------------------------------
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
+# def times_itself (num)
+#   return num * num
+# end
+
+# p times_itself(5)
 
 # ----------------------------------------------------------------------------------------
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
 
+# def first_L (string)
+#   return string[0]
+# end
 
+# p first_L("Heck Yea")
 
 
 # ----------------------------------------------------------------------------------------
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def combines (string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}"
+# end
 
+# p combines("Wow", "my", "dog")
 
 
 # ----------------------------------------------------------------------------------------
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+# def num_string(num)
+#   return num.to_s
+# end
+
+# p num_string(5)
 
 
 # ----------------------------------------------------------------------------------------
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-
+# def repeated (string)
+#   return string * 5
+# end
+# p repeated("cow")
 
 
 # ----------------------------------------------------------------------------------------
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def avg(num1, num2, num3)
+#   return (num1 + num2 + num3) / 3.0
+# end
 
-
-
-# ----------------------------------------------------------------------------------------
+# p avg(10, 5, 20)
+#----------------------------------------------------------------------------------------
 
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+
+# def times_ten(num)
+#   return (num * 10)
+# end
+
+# p times_ten(10)
 
 
 
