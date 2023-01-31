8de37971ef6475b35b5a5cbb7df84d4ea2cfcf94
Johnny Proano <jproano22@yahoo.com>
2023-01-31 16:33:19 -0500

    completed function practice in Javascript

--- functions.js
+++ functions.js
@@ -1,65 +1,95 @@
-
-
 // # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+// function timesTwo(num) {
+//   return num * 2;
+// }
 
+// console.log(timesTwo(10));
 
 // # ----------------------------------------------------------------------------------------
-
 // # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
+// function caps(string) {
+//   return string.toUpperCase();
+// }
 
+// console.log(caps("wow"));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+// function subtracted(num1, num2) {
+//   return num2 - num1;
+// }
 
+// console.log(subtracted(4, 2));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
+// function itself(num) {
+//   return num * num;
+// }
+
+// console.log(itself(10));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+// function firstLetter(string) {
+//   return string[0];
+// }
 
-
-
+// console.log(firstLetter("crazy"));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+// function strings(s1, s2, s3) {
+//   return `${s1} ${s2} ${s3}`;
+// }
 
-
+// console.log(strings("wow", "cool", "ok"));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+// function numString(num) {
+//   return num.toString();
+// }
 
-
+// console.log(numString(10));
 // # ----------------------------------------------------------------------------------------
 
 // # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-
-
+// function repeated(string) {
+//   return string + string + string + string + string;
+// }
+// console.log(repeated("cool"));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+// function avg(n1, n2, n3) {
+//   return (n1 + n2 + n3) / 3.0;
+// }
 
-
+// console.log(avg(5, 10, 20));
 
 // # ----------------------------------------------------------------------------------------
 
 // # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
 
-
+// function timesTen(num) {
+//   return num * 10 + 30;
+// }
+// console.log(timesTen(25));
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
