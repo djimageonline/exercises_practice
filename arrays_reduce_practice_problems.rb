1da878be5cec7ff6f0487dc371555be69896fa22
Johnny Proano <jproano22@yahoo.com>
2023-01-31 17:52:19 -0500

    completed 4 reduce problems in Ruby

--- arrays_reduce_practice_problems.rb
+++ arrays_reduce_practice_problems.rb
@@ -3,23 +3,38 @@
 #     For example, [5, 10, 8, 3] becomes 26.
 
 
-
+# nums = [5, 10, 8, 3]
+# total = nums.reduce(0) do |sum, num|
+#   sum + num
+# end
+# p total
 
 
 # ----------------------------------------------------------------------------------------------------------------
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
-
-
-
+# strings = ["volleyball", "basketball", "badminton"]
+# new_strings = strings.reduce do |combine, string|
+#   combine + string
+# end
+# p new_strings
 
 # ----------------------------------------------------------------------------------------------------------------
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
+# items = [
+#     {name: "chair", price: 100}, 
+#     {name: "pencil", price: 1}, 
+#     {name: "book", price: 4}
+# ]
 
+# total_sum = items.reduce(0) do |sum, item|
+#  item[:price] + sum
+# end
 
+# p total_sum
 
 # ----------------------------------------------------------------------------------------------------------------
 #  4. Start with an array of numbers and compute the the minumum number.
